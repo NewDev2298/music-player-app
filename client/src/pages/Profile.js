aa95f7938ce14fcd78515011ef7aa219059db0a8
Terrance Smith <terrancesmith@Laquishas-MacBook-Air.local>
2023-02-15 14:24:50 -0500

    added to click event to card component and more

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -70,118 +70,12 @@ const Profile = () => {
 
   return (
     <div className='container min-vh-100'>
-      <div>
         <h2 className='text-center mb-4'>
           Viewing {id ? `${user.username}'s` : 'Your'} Favorited Playlists.
         </h2>
-        {/* <main>
-          <div className='container min-vh-100'>
-            <div className='row d-flex justify-content-center'>
-              <div className='col-lg-3 col-md-5 col-sm-12 mx-1 card border-info mb-3'>
-                <h3 className='card-header'>Card Header</h3>
-                <div className='card-body'>
-                  <h5 className='card-title'>Special title treatment</h5>
-                  <h6 className='card-subtitle text-muted'>Support card subtitle</h6>
-                </div>
-                <img src='https://via.placeholder.com/150' alt='placeholder' className='d-block user-select-none' width='100%' height='200' aria-label='Placeholder: Image Cap'>
-                </img>
-                <div className="card-body">
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
-                  </p>
-                </div>
-                <div className="card-body d-flex justify-content-center">
-                  <a href="#" className="card-link align-self-center">Youtube link</a>
-                </div>
-              </div>
-              <div className='col-lg-3 col-md-5 col-sm-12 mx-1 card border-info mb-3'>
-                <h3 className='card-header'>Card Header</h3>
-                <div className='card-body'>
-                  <h5 className='card-title'>Special title treatment</h5>
-                  <h6 className='card-subtitle text-muted'>Support card subtitle</h6>
-                </div>
-                <img src='https://via.placeholder.com/150' alt='Image placeholder' className='d-block user-select-none' width='100%' height='200' aria-label='Placeholder: Image Cap'>
-                </img>
-                <div className="card-body">
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
-                  </p>
-                </div>
-                <div className="card-body d-flex justify-content-center">
-                  <a href="#" className="card-link align-self-center">Youtube link</a>
-                </div>
-              </div>
-              <div className='col-lg-3 col-md-5 col-sm-12 mx-1 card border-info mb-3'>
-                <h3 className='card-header'>Card Header</h3>
-                <div className='card-body'>
-                  <h5 className='card-title'>Special title treatment</h5>
-                  <h6 className='card-subtitle text-muted'>Support card subtitle</h6>
-                </div>
-                <img src='https://via.placeholder.com/150' alt='Image placeholder' className='d-block user-select-none' width='100%' height='200' aria-label='Placeholder: Image Cap'>
-                </img>
-                <div className="card-body">
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
-                  </p>
-                </div>
-                <div className="card-body d-flex justify-content-center">
-                  <a href="#" className="card-link align-self-center">Youtube link</a>
-                </div>
-              </div>
-              <div className='col-lg-3 col-md-5 col-sm-12 mx-1 card border-info mb-3'>
-                <h3 className='card-header'>Card Header</h3>
-                <div className='card-body'>
-                  <h5 className='card-title'>Special title treatment</h5>
-                  <h6 className='card-subtitle text-muted'>Support card subtitle</h6>
-                </div>
-                <img src='https://via.placeholder.com/150' alt='Image placeholder' className='d-block user-select-none' width='100%' height='200' aria-label='Placeholder: Image Cap'>
-                </img>
-                <div className="card-body">
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
-                  </p>
-                </div>
-                <div className="card-body d-flex justify-content-center">
-                  <a href="#" className="card-link align-self-center">Youtube link</a>
-                </div>
-              </div>
-              <div className='col-lg-3 col-md-5 col-sm-12 mx-1 card border-info mb-3'>
-                <h3 className='card-header'>Card Header</h3>
-                <div className='card-body'>
-                  <h5 className='card-title'>Special title treatment</h5>
-                  <h6 className='card-subtitle text-muted'>Support card subtitle</h6>
-                </div>
-                <img src='https://via.placeholder.com/150' alt='Image placeholder' className='d-block user-select-none' width='100%' height='200' aria-label='Placeholder: Image Cap'>
-                </img>
-                <div className="card-body">
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
-                  </p>
-                </div>
-                <div className="card-body d-flex justify-content-center">
-                  <a href="#" className="card-link align-self-center">Youtube link</a>
-                </div>
-              </div>
-              <div className='col-lg-3 col-md-5 col-sm-12 mx-1 card border-info mb-3'>
-                <h3 className='card-header'>Card Header</h3>
-                <div className='card-body'>
-                  <h5 className='card-title'>Special title treatment</h5>
-                  <h6 className='card-subtitle text-muted'>Support card subtitle</h6>
-                </div>
-                <img src='https://via.placeholder.com/150' alt='Image placeholder' className='d-block user-select-none' width='100%' height='200' aria-label='Placeholder: Image Cap'>
-                </img>
-                <div className="card-body">
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
-                  </p>
-                </div>
-                <div className="card-body d-flex justify-content-center">
-                  <a href="#" className="card-link align-self-center">Youtube link</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </main> */}
         <main>
-          <div>
             <Card />
-          </div>
         </main>
-      </div>
     </div>
   );
 };
