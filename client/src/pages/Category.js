5b5c475393162f631ae363971e64b6fcffe923b2
Chris <106836962+Cujion@users.noreply.github.com>
2023-02-16 14:12:34 -0500

    fixed margin on songs and profile page, updated font weight on smaller text

--- client/src/pages/Category.js
+++ client/src/pages/Category.js
@@ -63,30 +63,30 @@ const Category = () => {
       <h2 className='text-center mb-3'>
         Viewing {term} Category Songs
       </h2>
-      <div className="row d-flex justify-content-center">
+      <div className="row d-flex justify-content-center mx-1">
         {songs.map((song) => (
-            <div className='col-lg-3 col-md-6 col-sm-12 mx-1 card border-info mb-3'>
-              <h3 className='card-header'>{song.name}</h3>
-              <div className='card-body'>
-                <h5 className='card-title text-center'>Artist: {song.artist}</h5>
-                <h6 className='card-subtitle text-muted text-end'>{song.category}</h6>
-              </div>
-              <img src={`${process.env.PUBLIC_URL}/assets/${song.cover}`} className='img-fluid' width='100%' height='200' alt={song.name} />
-              <div className="card-body">
-                <p className="card-text text-center">Album: {song.album}
-                </p>
-              </div>
-              <div className="card-body d-flex  ">
-                <a href={song.video} className="card-link me-auto" target="_blank" rel="noreferrer" style={{ fontSize: "48px", margin: "0", padding: "0", color: "red" }}><AiFillYoutube /></a>
-                <button
-                  style={{ fontSize: "48px", margin: "0", padding: "0", color: "red", border: '0', background: 'none' }}
-                  onClick={
-                    () => user.songList.map(song => song._id).includes(song._id) ? handleRemove(song._id) : handleSave(song._id)
-                  }>
-                  {user.songList.map(song => song._id).includes(song._id) ? <AiFillHeart /> : <AiOutlineHeart />}
-                </button>
-              </div>
+          <div className='col-lg-3 col-md-6 col-sm-12 mx-1 card border-info mb-3'>
+            <h3 className='card-header'>{song.name}</h3>
+            <div className='card-body'>
+              <h5 className='card-title text-center'>Artist: {song.artist}</h5>
+              <h6 className='card-subtitle text-muted text-end'>{song.category}</h6>
             </div>
+            <img src={`${process.env.PUBLIC_URL}/assets/${song.cover}`} className='img-fluid' width='100%' height='200' alt={song.name} />
+            <div className="card-body">
+              <p className="card-text text-center" style={{ fontWeight: "100"}}>Album: {song.album}
+              </p>
+            </div>
+            <div className="card-body d-flex  ">
+              <a href={song.video} className="card-link me-auto" target="_blank" rel="noreferrer" style={{ fontSize: "48px", margin: "0", padding: "0", color: "red" }}><AiFillYoutube /></a>
+              <button
+                style={{ fontSize: "48px", margin: "0", padding: "0", color: "red", border: '0', background: 'none' }}
+                onClick={
+                  () => user.songList.map(song => song._id).includes(song._id) ? handleRemove(song._id) : handleSave(song._id)
+                }>
+                {user.songList.map(song => song._id).includes(song._id) ? <AiFillHeart /> : <AiOutlineHeart />}
+              </button>
+            </div>
+          </div>
         ))}
       </div>
     </div>);
